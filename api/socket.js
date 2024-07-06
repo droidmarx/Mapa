82f34e2fa0d158e0ff26ac6476599bac5145b45a
GUILHERME MARQUES SANTOS <99224826+droidmarx@users.noreply.github.com>
2024-07-05 23:48:19 -0300

    Update socket.js

--- api/socket.js
+++ api/socket.js
@@ -5,25 +5,25 @@ const { Server } = require('socket.io');
 const users = {};
 
 module.exports = (req, res) => {
- if (!res.socket.server.io) {
-  console.log('Setting up socket.io server...');
-  const io = new Server(res.socket.server);
-  res.socket.server.io = io;
+    if (!res.socket.server.io) {
+        console.log('Setting up Socket.IO server...');
+        const io = new Server(res.socket.server);
+        res.socket.server.io = io;
 
-  io.on('connection', (socket) => {
-   console.log('A user connected: ' + socket.id);
+        io.on('connection', (socket) => {
+            console.log('A user connected: ' + socket.id);
 
-   socket.on('updatePosition', (data) => {
-    users[socket.id] = data;
-    io.emit('positions', users);
-   });
+            socket.on('updatePosition', (data) => {
+                users[socket.id] = data;
+                io.emit('positions', users);
+            });
 
-   socket.on('disconnect', () => {
-    delete users[socket.id];
-    io.emit('positions', users);
-    console.log('A user disconnected: ' + socket.id);
-   });
-  });
- }
- res.end();
+            socket.on('disconnect', () => {
+                delete users[socket.id];
+                io.emit('positions', users);
+                console.log('A user disconnected: ' + socket.id);
+            });
+        });
+    }
+    res.end();
 };
