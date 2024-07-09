edaf33078d4cc8b89776aed8dec3508ce2d12f1e
GUILHERME MARQUES SANTOS <99224826+droidmarx@users.noreply.github.com>
2024-07-09 12:33:17 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,53 +1,68 @@
 const Ably = require('ably');
 const fs = require('fs');
 const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 const ably = new Ably.Realtime('ynrGug.SXDJOg:aoStJmyUcLlpwgEzYsJt8CFDsTBCq-yMjqNn6_DkgvM');
 const channel = ably.channels.get('chat-demo');
 
-module.exports = (req, res) => {
-  if (req.method === 'GET' && req.query.endpoint === 'auth') {
-    ably.auth.createTokenRequest((err, tokenRequest) => {
-      if (err) {
-        res.status(500).send('Erro ao criar token de autenticação');
-      } else {
-        res.json(tokenRequest);
-      }
-    });
-  } else if (req.method === 'GET' && req.query.endpoint === 'users') {
-    fs.readFile(path.join(__dirname, '../users.json'), (err, data) => {
-      if (err) {
-        res.status(500).send('Erro ao ler lista de usuários');
-      } else {
-        res.json(JSON.parse(data));
-      }
-    });
-  } else if (req.method === 'POST' && req.query.endpoint === 'update-location') {
-    const { username, lat, lng } = req.body;
-    fs.readFile(path.join(__dirname, '../users.json'), (err, data) => {
-      if (err) {
-        res.status(500).send('Erro ao ler lista de usuários');
-      } else {
-        const users = JSON.parse(data);
-        const userIndex = users.findIndex(user => user.username === username);
+// Endpoint para autenticação
+app.get('/api/auth', (req, res) => {
+  ably.auth.createTokenRequest((err, tokenRequest) => {
+    if (err) {
+      res.status(500).send('Erro ao criar token de autenticação');
+    } else {
+      res.json(tokenRequest);
+    }
+  });
+});
 
-        if (userIndex !== -1) {
-          users[userIndex].lat = lat;
-          users[userIndex].lng = lng;
+// Endpoint para obter a lista de usuários
+app.get('/api/users', (req, res) => {
+  fs.readFile(path.join(__dirname, '../users.json'), (err, data) => {
+    if (err) {
+      res.status(500).send('Erro ao ler lista de usuários');
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
+
+// Endpoint para atualizar a localização do usuário
+app.post('/api/update-location', (req, res) => {
+  const { username, lat, lng } = req.body;
+  fs.readFile(path.join(__dirname, '../users.json'), (err, data) => {
+    if (err) {
+      res.status(500).send('Erro ao ler lista de usuários');
+    } else {
+      const users = JSON.parse(data);
+      const userIndex = users.findIndex(user => user.username === username);
+
+      if (userIndex !== -1) {
+        users[userIndex].lat = lat;
+        users[userIndex].lng = lng;
+      } else {
+        users.push({ username, lat, lng });
+      }
+
+      fs.writeFile(path.join(__dirname, '../users.json'), JSON.stringify(users), (err) => {
+        if (err) {
+          res.status(500).send('Erro ao atualizar lista de usuários');
         } else {
-          users.push({ username, lat, lng });
+          res.send('Localização atualizada com sucesso');
         }
+      });
+    }
+  });
+});
 
-        fs.writeFile(path.join(__dirname, '../users.json'), JSON.stringify(users), (err) => {
-          if (err) {
-            res.status(500).send('Erro ao atualizar lista de usuários');
-          } else {
-            res.send('Localização atualizada com sucesso');
-          }
-        });
-      }
-    });
-  } else {
-    res.status(404).send('Endpoint não encontrado');
-  }
-};
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
